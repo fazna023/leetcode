8d4b88930c5c18fd3e7c89d974d864098b028ffb
fazna023 <fasnafasy22@gmail.com>
2025-01-03 09:59:50 +0530

    Time: 69 ms (10.33%), Space: 49.6 MB (82.41%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.js
+++ 0137-single-number-ii/0137-single-number-ii.js
@@ -3,17 +3,10 @@
  * @return {number}
  */
 var singleNumber = function(nums) {
-    for(let i=0;i<nums.length;i++){
-      let count=0;
-      for(let j=0;j<nums.length;j++){
-          if(i!=j && nums[i]==nums[j]){
-                  count++;
-                  break;
-          }
-          
-      }
-      if(count==0){
-         return nums[i];
-      }
+    for (let i = 0; i < nums.length; i++) {
+        if (nums.indexOf(nums[i]) == nums.lastIndexOf(nums[i])) {
+            return nums[i]
+            break
+        }
     }
 };
