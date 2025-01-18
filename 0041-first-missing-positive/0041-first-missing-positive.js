6d66b2b52667537c5eaf21384bcdb2a1ae32071b
fazna023 <fasnafasy22@gmail.com>
2025-01-18 13:34:48 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.js
+++ 0041-first-missing-positive/0041-first-missing-positive.js
@@ -3,13 +3,5 @@
  * @return {number}
  */
 var firstMissingPositive = function(nums) {
-        let x=1;
-          nums.sort((a, b) => a-b);
-        for( i=0;i<nums.length; i++) {
-        if(nums[i] == x){
-            x++;  
-        }
-         
-    }
-    return x
+    
 };
