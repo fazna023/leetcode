9ce397b5bc22a0b27972d2ed1377a361bc20b5a9
fazna023 <fasnafasy22@gmail.com>
2025-01-21 10:14:27 +0530

    Time: 10 ms (33.99%), Space: 59.2 MB (49.46%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.js
+++ 0326-power-of-three/0326-power-of-three.js
@@ -3,10 +3,6 @@
  * @return {boolean}
  */
 var isPowerOfThree = function(n) {
-    if(n<=0)
-    return false;
-    while(n%3==0){
-        n=n/3
-    }
-    return n==1;
+    while(n>1) n = n/3
+    return n===1
 };
